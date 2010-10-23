dcc8265ec4ebc841d44e1f7c103ef58d5a461f67
Michael Schade <michaelschade@sddomain.com>
2010-10-23 17:39:03 -0500

    Fixed 'cannot execute binary file.'

--- AccentuateUs.m
+++ AccentuateUs.m
@@ -6,8 +6,37 @@
 //  Copyright 2010 Spearhead Development LLC. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import "AccentuateUs.h"
 
 @implementation AccentuateUs
 
+- (void)accentuate:(NSPasteboard *) pboard
+        userData:(NSString *) data
+        error:(NSString **)error {
+    NSArray *types = [pboard types];
+    types = [NSArray arrayWithObject:NSStringPboardType];
+    [pboard declareTypes:types owner:nil];
+    [pboard setString:@"AA" forType:NSStringPboardType];
+    return;
+}
+
 @end
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    AccentuateUs *service = [[AccentuateUs alloc] init];
+    NSRegisterServicesProvider(service, @"AccentuateUs");
+    
+    NSLog(@"Test.");
+    
+    NS_DURING
+    [[NSRunLoop currentRunLoop] run];
+    NS_HANDLER
+    NSLog(@"%@", localException);
+    NS_ENDHANDLER
+    
+    [service release];
+    [pool release];
+    return 0;
+}
--- main.m
+++ main.m
@@ -6,21 +6,3 @@
 //  Copyright 2010 Spearhead Development LLC. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "AccentuateUs.h"
-
-int main(int argc, char *argv[]) {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    AccentuateUs *service = [[AccentuateUs alloc] init];
-    NSRegisterServicesProvider(service, @"AccentuateUs");
-    
-    NS_DURING
-        [[NSRunLoop currentRunLoop] run];
-    NS_HANDLER
-        NSLog(@"%@", localException);
-    NS_ENDHANDLER
-    
-    [service release];
-    [pool release];
-    return 0;
-}
